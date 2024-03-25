ff58425704a57a9940fc653a07706eb52cb690b1
alreadycow <alreadycow_@naver.com>
2024-03-25 14:54:18 +0900

    Initial commit

---
+++ healing/src/test/java/misoya/healing/HealingApplicationTests.java
@@ -0,0 +1,13 @@
+package misoya.healing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
